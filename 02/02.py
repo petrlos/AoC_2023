daf1c247277d04d869b226ad4836d8e6bb0f393f
Petr Los <petrlos@gmail.com>
2023-12-02 13:13:16 +0100

    Day 2: both stars working - final

--- 02/02.py
+++ 02/02.py
@@ -1,54 +1,34 @@
 #Advent of Code 2023: Day 2
+import re
+
 class Bag:
-    def __init__(self):
-        self.red = []
-        self.green = []
-        self.blue = []
+    def __init__(self, colours):
+        self.red, self.green, self.blue = colours
 
     @property
     def bag_possible(self):
-        return (self.red_max <= 12) and (self.green_max <= 13) and (self.blue_max <= 14)
-
-    @property
-    def red_max(self):
-        return max(self.red)
-
-    @property
-    def green_max(self):
-        return max(self.green)
-
-    @property
-    def blue_max(self):
-        return max(self.blue)
+        return (self.red <= 12) and (self.green <= 13) and (self.blue <= 14)
 
     @property
     def power(self):
-        return self.red_max * self.blue_max * self.green_max
+        return self.red * self.blue * self.green
 
-def get_cubes_per_game(line):
-    current_bag = Bag()
-    line = line.split(": ")[1]
-    turns = line.split("; ")
-    for turn in turns:
-        cubes = turn.split(", ")
-        for cube in cubes:
-            count, colour = cube.split(" ")
-            if colour == "blue":
-                current_bag.blue.append(int(count))
-            elif colour == "red":
-                current_bag.red.append(int(count))
-            elif colour == "green":
-                current_bag.green.append(int(count))
-    return current_bag
+def parsing(line):
+    regexes = [re.compile(r"(\d+) red"), re.compile(r"(\d+) green"), re.compile(r"(\d+) blue")]
+    colours = []
+    for regex in regexes:
+        max_count = max(list(map(int, regex.findall(line))))
+        colours.append(max_count)
+    return colours
 
 #MAIN
-with open("test.txt") as file:
+with open("data.txt") as file:
     lines = file.read().splitlines()
 
 bag_ids = []
 powers = []
 for game, line in enumerate(lines, start = 1):
-    new_bag = get_cubes_per_game(line)
+    new_bag = Bag(parsing(line))
     if new_bag.bag_possible:
         bag_ids.append(game)
     powers.append(new_bag.power)
