3c3b6f17eab7ff94c5837c3d6da499b75d2b6a45
Petr Los <petrlos@gmail.com>
2023-12-02 12:59:16 +0100

    Day 2: both stars working, Class uses properties

--- 02/02.py
+++ 02/02.py
@@ -4,27 +4,26 @@ class Bag:
         self.red = []
         self.green = []
         self.blue = []
-        self.red_max = 0
-        self.green_max = 0
-        self.blue_max = 0
-        self.power = 0
 
-    def get_max(self):
-        self.red_max = max(self.red)
-        self.green_max = max(self.green)
-        self.blue_max = max(self.blue)
-
+    @property
     def bag_possible(self):
-        if self.red_max > 12:
-            return False
-        elif self.green_max > 13:
-            return False
-        elif self.blue_max > 14:
-            return False
-        return True
+        return (self.red_max <= 12) and (self.green_max <= 13) and (self.blue_max <= 14)
 
-    def sum_up(self):
-        self.power = self.red_max * self.blue_max * self.green_max
+    @property
+    def red_max(self):
+        return max(self.red)
+
+    @property
+    def green_max(self):
+        return max(self.green)
+
+    @property
+    def blue_max(self):
+        return max(self.blue)
+
+    @property
+    def power(self):
+        return self.red_max * self.blue_max * self.green_max
 
 def get_cubes_per_game(line):
     current_bag = Bag()
@@ -40,21 +39,19 @@ def get_cubes_per_game(line):
                 current_bag.red.append(int(count))
             elif colour == "green":
                 current_bag.green.append(int(count))
-    current_bag.get_max()
-    current_bag.sum_up()
     return current_bag
 
 #MAIN
-with open("data.txt") as file:
+with open("test.txt") as file:
     lines = file.read().splitlines()
 
-bags = []
+bag_ids = []
 powers = []
 for game, line in enumerate(lines, start = 1):
     new_bag = get_cubes_per_game(line)
-    if new_bag.bag_possible():
-        bags.append(game)
+    if new_bag.bag_possible:
+        bag_ids.append(game)
     powers.append(new_bag.power)
 
-print("Part 1:",sum(bags))
+print("Part 1:",sum(bag_ids))
 print("Part 2:",sum(powers))
