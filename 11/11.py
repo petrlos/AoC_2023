b1ee5a7761744424d8732ca08d5b6701f19fa5fd
Petr Los <petrlos@gmail.com>
2023-12-11 21:29:33 +0100

    Day 11: Both stars cleaned

--- 11/11.py
+++ 11/11.py
@@ -12,6 +12,23 @@ def create_space(lines):
                 space.add((x,y))
     return space, x, y
 
+def count_distances_between_stars(multiplikator):
+    pairs = combinations(space, 2)
+    distances = []
+    for pair in pairs:
+        first, second = pair
+        x1, y1 = first
+        x2, y2 = second
+        if x1 > x2:
+            x1, x2 = x2, x1
+        if y1 > y2:
+            y1, y2 = y2, y1
+        delta_x = list(filter(lambda delta: x1 < delta < x2, empty_x))
+        delta_y = list(filter(lambda delta: y1 < delta < y2, empty_y))
+        distances.append(ext_manh_distance(first, second) +
+                         len(delta_x) * (multiplikator-1) + len(delta_y) * (multiplikator-1))
+    return sum(distances)
+
 #MAIN
 with open("data.txt") as file:
     lines = file.read().splitlines()
@@ -28,18 +45,5 @@ for star in space:
     if y in empty_y:
         empty_y.remove(y)
 
-pairs = combinations(space, 2)
-distances = []
-for pair in pairs:
-    first, second = pair
-    x1, y1 = first
-    x2, y2 = second
-    if x1 > x2:
-        x1, x2 = x2, x1
-    if y1 > y2:
-        y1, y2 = y2, y1
-    delta_x = list(filter(lambda delta: x1 < delta < x2, empty_x))
-    delta_y = list(filter(lambda delta: y1 < delta < y2, empty_y))
-    distances.append(ext_manh_distance(first, second) + len(delta_x)*999999 + len(delta_y)*999999)
-
-print(sum(distances))
+print("Part 1:", count_distances_between_stars(2))
+print("Part 2:", count_distances_between_stars(1000000))
