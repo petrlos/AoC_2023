681be3e3b1f493fca5309459fa43e57b50775b90
Petr Los <petrlos@gmail.com>
2023-12-20 21:45:08 +0100

    Day 16: Both stars

--- 16/16.py
+++ 16/16.py
@@ -38,6 +38,32 @@ def out_of_grid(coord, max_x, max_y):
 def tuple_sum(a,b):
     return tuple([x + y for x, y in zip(a,b)])
 
+def count_energized():
+    energized = set()  # only points, that have been energized
+    queue = deque([[(-1,0), 3]])  # beam location, direction
+    queue_visited = []
+
+    while queue:
+        current_coord, beam_direction = queue.popleft()
+        new_coord = tuple_sum(current_coord, directions[beam_direction])
+        if not out_of_grid(new_coord, max_x, max_y):
+            if new_coord in grid.keys():  # direction might change
+                if beam_direction in [1, 3]:  # beam moving east/west
+                    new_directions_delta = beam_e_w[grid[new_coord]]
+                else:  # beam moving north/south
+                    new_directions_delta = beam_n_s[grid[new_coord]]
+                for new_direction_delta in new_directions_delta:  # split beams might result in two beams
+                    new_direction = (beam_direction + new_direction_delta) % 4
+                    queue.append([new_coord, new_direction])
+            else:
+                queue.append([new_coord, beam_direction])  # direction of beam does not change
+            energized.add(new_coord)  # light up new point
+            if queue[-1] in queue_visited:  # check if beam with coords and direction has not been calculated already
+                queue.pop()
+            else:
+                queue_visited.append(queue[-1])
+    return len(energized)
+
 #MAIN
 with open("data.txt") as file:
     lines = file.read().splitlines()
@@ -49,29 +75,24 @@ beam_e_w = {"-":[0], "|":[+1,-1], "\\":[ -1], "/":[ +1]}
 beam_n_s = {"-":[+1,-1], "|":[0], "\\":[ +1], "/":[ -1]}
 directions = [(0, -1), (-1, 0), (0, 1), (1, 0)]  #NWSE = 0,1,2,3
 
-energized = set() #only points, that have been energized
-queue = deque([[(-1,0),3]]) #beam location, direction
-queue_visited = []
+part1 = count_energized()
+print("Part 1:",part1)
 
-while queue:
-    current_coord, beam_direction = queue.popleft()
-    new_coord = tuple_sum(current_coord, directions[beam_direction])
-    if not out_of_grid(new_coord, max_x, max_y) :
-        if new_coord in grid.keys(): #direction might change
-            if beam_direction in [1,3]: #beam moving east/west
-                new_directions_delta = beam_e_w[grid[new_coord]]
-            else: #beam moving north/south
-                new_directions_delta = beam_n_s[grid[new_coord]]
-            for new_direction_delta in new_directions_delta: #split beams might result in two beams
-                new_direction = (beam_direction+new_direction_delta) % 4
-                queue.append([new_coord, new_direction])
-        else:
-            queue.append([new_coord, beam_direction]) #direction of beam does not change
-        energized.add(new_coord) #light up new point
-        if queue[-1] in queue_visited: #check if beam with coords and direction has not been calculated already
-            queue.pop()
-        else:
-            queue_visited.append(queue[-1])
+#task 2 counted manually using cycle and changing input parameters in function
+# see comments below
 
-print("Part 1:",len(energized))
+"""maxes = []
+for counter, i in enumerate( coordinates range ):
+    result = count_energized(i, direction)
+    print(counter, result)
+    maxes.append(result)
+print(max(maxes))
+"""
+
+#left border:(-1,0) .. (-1,max_y), direction 3: result 7180
+#top border: (0,-1) .. (max_x, -1), direction 2: result 7679
+#right border: (max_x+1, 0) .. (max_x + 1, max_y): direction 1: result 7759
+#bottom border: not needed, max was on right border
+
+print("Part 2:", 7759)
 print("Runtime:", datetime.now() - time_start)
