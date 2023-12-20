e39caaf64ffa99e5a50cd83b29a4b8f711fe2373
Petr Los <petrlos@gmail.com>
2023-12-20 23:50:56 +0100

    Day 13: Both stars, old and new version mixe up

--- 13/13.py
+++ 13/13.py
@@ -11,74 +11,44 @@ def generate_numpy_grid(pattern):
     number_grid = np.array(number_grid)
     return number_grid
 
-def generate_columns(midpoint, width):
-    #returns indexes of corresponding columns for comparison
-    #e.g. mirrors between 3/4, must compare 3/4, 2/5, 1/6 and 0/7
-    left = list(range(midpoint, 0, -1))
-    right = list(range(midpoint+1, width+1))
-    return left, right
-
-def compare_columns(left_col_index, right_col_index, current_grid):
-    #if left or right runs out, for cycle ends
-    for col1, col2 in zip(left_col_index, right_col_index):
-        left = current_grid[:, col1-1]
-        right = current_grid[:, col2-1]
-        if not np.array_equal(left, right):
-            return False
-    return True
-
-def find_column_of_reflection(current_grid, width):
-    result = -1 # no reflection found
-    for midpoint in range(1, width): #1 == reflects between 0/1, etc.
-        left_col_index, right_col_index = generate_columns(midpoint, width)
-        if compare_columns(left_col_index, right_col_index, current_grid):
-            return midpoint #no more comparison needed
-    return result
-
-#MAIN
-with open("data.txt") as file:
-    patterns = file.read().split("\n\n")
-
-results = []
-for pattern in patterns:
-    current_grid = generate_numpy_grid(pattern) #create a numpy field of zeros and ones
-    result = find_column_of_reflection(current_grid, current_grid.shape[1])
-    if result == -1: #no result found - rotate by 90 deg counterCW and search again
-        current_grid = np.rot90(current_grid, k=1)
-        result = 100*find_column_of_reflection(current_grid, current_grid.shape[1])
-    results.append(result)
-print("Part 1:",sum(results))
-
-def find_smudge(current_grid):
+def find_smudge(current_grid, difference):
     width = current_grid.shape[1]
     for midpoint in range(1, width):
         left = current_grid[:, :midpoint]  # cut array left from midpoint
-        right = current_grid[:, midpoint:]
+        right = current_grid[:, midpoint:] # cut array right from midpoint
         left_width, right_width = left.shape[1], right.shape[1]
-        cut_off = abs(left_width - right_width)
+        cut_off = abs(left_width - right_width) #both arrays must have same width
         if left_width < right_width:
             right = right[:, :-cut_off]
         else:
             left = left[:, cut_off:]
         left = left[:, ::-1]  # reverse order of columns
         diff = abs(left - right)
-        if np.count_nonzero(diff == 1) == 1:  # if exactly one 1 remains - differ in point
+        #Part1: difference must be zero - arrays are completely the same
+        #Part2: number "1" may be only once in the difference of arrays
+        if np.count_nonzero(diff == 1) == difference:
             return midpoint
     return -1
 
-"""
-actually is only "find_smudge" for task1 and task2 needed - only difference to be made is on 
-line 65: if np.count_nonzero(diff == 1) == 0:
-if all numbers in difference of two numpy arrays are zeros, the arrays are the same
-"""
+#MAIN
+with open("data.txt") as file:
+    patterns = file.read().split("\n\n")
 
-results = []
+part1s, part2s = [], []
 for pattern in patterns:
     current_grid = generate_numpy_grid(pattern)
-    result = find_smudge(current_grid)
-    if result == -1:
-        current_grid = np.rot90(current_grid, k=1)
-        result = 100 * find_smudge(current_grid)
-    results.append(result)
-print("Part 2:",sum(results))
+    part1 = find_smudge(current_grid, 0)
+    part2 = find_smudge(current_grid, 1)
+    if part1 == -1: #no midpoint found
+        rotated = np.rot90(current_grid, k=1)
+        part1 = 100 * find_smudge(rotated, 0)
+    if part2 == -1: #no smudge found
+        rotated = np.rot90(current_grid, k=1)
+        part2 = 100 * find_smudge(rotated, 1)
+    part1s.append(part1)
+    part2s.append(part2)
+
+print("Part 1", sum(part1s))
+print("Part 2", sum(part2s))
+
 print("Runtime:", datetime.now() - time_start)
