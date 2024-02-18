2a188ca47fb4121c4015fbdd7324769c817be1dc
Petr Los <petrlos@gmail.com>
2024-02-18 20:43:57 +0100

    Day 17: Refactored, some errors corrected, added functions, still not working

--- 17/17.py
+++ 17/17.py
@@ -11,35 +11,38 @@ def parse_grid(lines):
             grid[(r,c)] = int(num)
     return grid, r, c
 
+def find_path(grid):
+    Status = namedtuple("Status", ["thl", "row", "col", "dir_index", "steps"])
+    # total heat loss, row, col, direction, steps
+    start = Status(0, 0, 0, 1, 0)  # start direction down - may "continue" down or change right
+
+    heap = []
+    heappush(heap, start)
+    visited = set()
+
+    while heap:
+        current = heappop(heap)  # get from heap step with least THL
+        for delta_dir in [-1, 0, 1]:  # turn left, carry on, turn right
+            new_direction = (delta_dir + current.dir_index) % 4
+            delta_row, delta_col = directions[new_direction]
+            row = delta_row + current.row  # get new row and col coords
+            col = delta_col + current.col
+            steps = current.steps + 1 if delta_dir == 0 else 1  # if turn steps = 1, else +1
+            if (row, col) in grid.keys():  # inside a grid
+                new_status = Status(current.thl + grid[row, col], row, col, new_direction, steps)
+                checker = new_status[1:]
+                if steps <= 3 and checker not in visited:
+                    visited.add(checker)
+                    heappush(heap, new_status)
+                if row == max_r and col == max_c:
+                    return new_status.thl
+
 #MAIN
-with open("test.txt") as file:
+with open("data.txt") as file:
     lines = file.read().splitlines()
 
 directions = [(0,1), (1,0), (0,-1), (-1,0)] #RDLU
 grid, max_r, max_c = parse_grid(lines)#row, column
-Status = namedtuple("Status", ["thl", "row", "col", "dir_index", "steps"])
 
-#total heat loss, row, col, direction, steps
-start = Status(0, 0, 0, 0, 0) #start direction down - may "continue" down or change right
-
-heap = []
-heappush(heap, start)
-visited = set()
-
-while heap:
-    current = heappop(heap) #get from heap step with least THL
-    for delta_dir in [-1, 0, 1]: #turn left, carry on, turn right
-        new_direction = (delta_dir + current.dir_index) % 4
-        delta_row, delta_col = directions[new_direction]
-        row = delta_row + current.row #get new row and col coords
-        col = delta_col + current.col
-        steps = current.steps + 1 if delta_dir == 0 else 1 #if turn steps = 1, else +1
-        if (row, col) in grid.keys(): #inside a grid
-            new_status = Status(current.thl+grid[row,col], row, col, new_direction, steps)
-            checker = new_status[1:]
-            if steps <= 3 or checker not in visited:
-                #ic(new_status)
-                heappush(heap, new_status)
-            if row == max_r and col == max_c:
-                print(new_status.thl)
-
+result = find_path(grid)
+print(result)
