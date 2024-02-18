f79c0ef446ac75ec64f26e91ecbe01c9822ab0f8
Petr Los <petrlos@gmail.com>
2024-02-18 22:02:01 +0100

    Day 17: Added path-tracking, still not correct result

--- 17/17.py
+++ 17/17.py
@@ -12,13 +12,14 @@ def parse_grid(lines):
     return grid, r, c
 
 def find_path(grid):
-    Status = namedtuple("Status", ["thl", "row", "col", "dir_index", "steps"])
+    Status = namedtuple("Status", ["thl", "row", "col", "dir_index", "steps", "path"])
     # total heat loss, row, col, direction, steps
-    start = Status(0, 0, 0, 1, 0)  # start direction down - may "continue" down or change right
+    start = Status(0, 0, 0, 0, 0, set())  # start direction down - may "continue" down or change right
 
     heap = []
     heappush(heap, start)
     visited = set()
+    results = []
 
     while heap:
         current = heappop(heap)  # get from heap step with least THL
@@ -28,21 +29,26 @@ def find_path(grid):
             row = delta_row + current.row  # get new row and col coords
             col = delta_col + current.col
             steps = current.steps + 1 if delta_dir == 0 else 1  # if turn steps = 1, else +1
+            path = set()
+            path = path | (current.path)
+            path.add((row, col))
             if (row, col) in grid.keys():  # inside a grid
-                new_status = Status(current.thl + grid[row, col], row, col, new_direction, steps)
-                checker = new_status[1:]
+                new_status = Status(current.thl + grid[row, col], row, col, new_direction, steps, path)
+                checker = new_status[1:-1]
                 if steps <= 3 and checker not in visited:
                     visited.add(checker)
                     heappush(heap, new_status)
                 if row == max_r and col == max_c:
-                    return new_status.thl
+                    results.append(new_status)
+        if len(results) > 2:
+            return results
 
-#MAIN
-with open("data.txt") as file:
+#MAIN - part1 should return 928
+with open("test.txt") as file:
     lines = file.read().splitlines()
 
 directions = [(0,1), (1,0), (0,-1), (-1,0)] #RDLU
 grid, max_r, max_c = parse_grid(lines)#row, column
 
 result = find_path(grid)
-print(result)
+ic(result)
