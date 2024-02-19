9f4dabc341f99b2fc03883b8af8af1cc1a4017b1
Petr Los <petrlos@gmail.com>
2024-02-19 22:50:04 +0100

    Day 17: 1st star working

--- 17/17.py
+++ 17/17.py
@@ -2,7 +2,8 @@
 from heapq import heappop, heappush
 from collections import namedtuple
 from icecream import ic
-import time
+from datetime import datetime
+time_start = datetime.now()
 
 def parse_grid(lines):
     grid = {} #heat_loss on that point
@@ -29,26 +30,23 @@ def find_path(grid):
             row = delta_row + current.row  # get new row and col coords
             col = delta_col + current.col
             steps = current.steps + 1 if delta_dir == 0 else 1  # if turn steps = 1, else +1
-            path = set()
-            path = path | (current.path)
-            path.add((row, col))
+            path = current.path | {(row, col)}
             if (row, col) in grid.keys():  # inside a grid
                 new_status = Status(current.thl + grid[row, col], row, col, new_direction, steps, path)
                 checker = new_status[1:-1]
                 if steps <= 3 and checker not in visited:
                     visited.add(checker)
                     heappush(heap, new_status)
-                if row == max_r and col == max_c:
-                    results.append(new_status)
-        if len(results) > 2:
-            return results
+                    if row == max_r and col == max_c:
+                        return new_status
 
-#MAIN - part1 should return 928
-with open("test.txt") as file:
+Added path-tracking, still not correct result#MAIN - part1 should return 928
+with open("data.txt") as file:
     lines = file.read().splitlines()
 
 directions = [(0,1), (1,0), (0,-1), (-1,0)] #RDLU
 grid, max_r, max_c = parse_grid(lines)#row, column
 
 result = find_path(grid)
-ic(result)
+print("Part 1:", result.thl)
+print(f"Total runtime: {datetime.now() - time_start}")
